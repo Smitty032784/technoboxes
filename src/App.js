cab6065cf662ac00d3f645af1b75588d4769bd7c
Smitty032784 <68964091+Smitty032784@users.noreply.github.com>
2020-11-24 17:09:17 -0500

    Technobox App!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Box extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         bgColor: ""
+      };
+   }
+   
+   changeColor = (event) => {
+      let randomColor = Math.floor(Math.random()*16777215).toString(16);
+      this.setState({bgColor: "#" + randomColor}) 
+   }
+
+   render() {
+      return (
+         <div className="box" onMouseOver={() => this.changeColor()} style={{backgroundColor: this.state.bgColor}}></div>
+      );
+   }   
+}
+
+class App extends React.Component {
+   render() {
+      const boxes = [];
+      for(let i = 0; i < 104; i++)
+      {
+         boxes.push(<Box></Box>)
+      }
+      return (
+         <div>
+            {boxes}
+         </div>
+      );
+   }
 }
 
 export default App;
